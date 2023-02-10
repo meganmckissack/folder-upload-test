7f2f5f75bd3b47f37801298e70ec5183d721647a
Megan McKissack <meganmckissack@gmail.com>
2023-02-10 14:12:17 -0800

    emojis

--- sketch.js
+++ sketch.js
@@ -1,15 +1,12 @@
 function setup() {
   createCanvas(400, 400);
-  background(220);
+   background(220);
 }
 
 function draw() {
-  
-  
-   fill(100);
-  for(let i = 0; i <= width; i+= 20) {
-    for(let j = 0; j <= height; j+= 20) {
-      circle(mouseX, mouseY, 10);
-    }
-    }
+ // background(220);
+  textSize(64);
+  textAlign(CENTER)
+  text('😜😂😍', mouseX, mouseY);
+  text('☀️🌬️🛬', width/2, height/2 + 100);
 }
