f4b553af167e8bc8d8c9a380dd67a9934004fb79
Megan McKissack <meganmckissack@gmail.com>
2023-02-10 14:05:58 -0800

    add loop

--- sketch.js
+++ sketch.js
@@ -1,7 +1,14 @@
 function setup() {
   createCanvas(400, 400);
+  background(220);
 }
 
 function draw() {
-  background(220);
+  
+  
+   fill(100);
+  for(let i = 0; i <= width; i+= 20) {
+    for(let j = 0; j <= height; j+= 20) {
+      circle(mouseX, mouseY, 10);
+    }
 }
